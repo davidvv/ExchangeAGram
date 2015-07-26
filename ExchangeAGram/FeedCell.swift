8d5adedba25094c95b48c46d5741d1d1ca624729
davidvv <idavidvv@gmail.com>
2015-07-26 22:14:31 +0200

    ExchangeAGram
    proyecto ExchangeAGram de bitfountain. Primer commit.

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by David Vences Vaquero on 30/6/15.
+//  Copyright (c) 2015 David. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
