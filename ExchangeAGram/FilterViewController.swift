f9b9162a693a76216ee3c84a23d4a86a4bab864a
davidvv <idavidvv@gmail.com>
2015-08-23 17:32:30 +0200

    peta al cargar los filtros

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -18,6 +18,8 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     let kIntensity = 0.7 //El valor sepia
     
     var context:CIContext = CIContext(options: nil)
+    
+    var filters:[CIFilter] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +43,8 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         
         self.view.addSubview(collectionView)
         
+        filters = photoFilters()
+        
         
     }
 
@@ -52,14 +56,16 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     //UICollectionviewDataSource
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return filters.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell:FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as! FilterCell
         
-        cell.imageView.image = UIImage(named: "Placeholder")
+//        cell.imageView.image = UIImage(named: "Placeholder")
+        
+        cell.imageView.image = filteredImageFromImage(thisFeedItem.image, filter: filters[indexPath.row])
         
         return cell
     }
